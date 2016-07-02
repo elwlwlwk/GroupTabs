edfb165f0a65dfe9bea26729e7a7e6d94b62e52a
wisewolf <holo@wisewolf.org>
2016-07-02 22:56:53 +0900

    added render function and applied bootstrap

--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
 //chrome.tabs.create({"url":"test","selected":true}, function(tab){});
+
+function void_func(){};
+
+function Tab(url){
+	this.url= url;
+}
 tab_group={};
 cur_group_idx=0;
 
@@ -22,23 +28,31 @@ function save_tab_group(){
 	chrome.storage.local.set({"tab_group": tab_group, "cur_group_idx": cur_group_idx});
 }
 
-function load_tab_group(){
+function update_cur_tab_group(after= void_func){
+	chrome.tabs.query({"currentWindow": true}, function(tabs){
+		var tab_list=[];
+		tabs.forEach(function(tab){
+			tab_list.push(new Tab(tab.url))
+		})
+		tab_group[cur_group_idx]= {"group_name":cur_group_idx, "tab_list":tab_list};
+		after();
+	})
+}
+
+function load_tab_group(after= void_func){
 	chrome.storage.local.get("tab_group", function(result){
 		tab_group= result["tab_group"];
 		if(!tab_group){
 			cur_group_idx=0;
-			chrome.tabs.query({"currentWindow": true}, function(tabs){
-				url_list=[];
-				tabs.forEach(function(tab){
-					url_list.push(tab.url);
-				})
-				tab_group={};
-				tab_group[cur_group_idx]= url_list;
-			})
+			tab_group={};
 		}
 		chrome.storage.local.get("cur_group_idx", function(result){
-		cur_group_idx= result["cur_group_idx"];
+			cur_group_idx= result["cur_group_idx"];
+			if(!cur_group_idx){
+				cur_group_idx= 0;
+			}
 		});
+		after();
 	});
 }
 
@@ -50,9 +64,46 @@ function close_tabs(tab_list){
 
 function restore_tab_group(group_idx){
 	chrome.tabs.query({"currentWindow": true}, function(tabs){
-		tab_group[group_idx].forEach(function(url){
+		tab_group[group_idx]["tab_list"].forEach(function(url){
 			chrome.tabs.create({"url":url}, function(tab){});
 		});//restore tab
 		close_tabs(tabs);
 	});
 }
+
+function render_tab_group(group_idx){
+	var tabs= tab_group[group_idx]["tab_list"];
+	var tab_list= document.getElementById("tab_list");
+	var ul_tab_groups= document.getElementById("tab_groups");
+
+	while(tab_list.firstChild){
+		tab_list.removeChild(tab_list.firstChild);
+	}
+
+	for(var idx in tab_group){
+		var new_li= document.createElement("li");
+		var new_a= document.createElement("a");
+		var group_name= document.createTextNode(tab_group[idx]["group_name"]);
+
+		new_a.appendChild(group_name);
+		new_li.appendChild(new_a);
+		ul_tab_groups.appendChild(new_li);
+	}
+
+	tabs.forEach(function(tab){
+		var new_li= document.createElement("li");
+		var tab_txt= document.createTextNode(tab.url);
+		new_li.appendChild(tab_txt);
+		tab_list.appendChild(new_li);
+	})
+}
+
+function render_popup(){
+	load_tab_group(function(){
+		update_cur_tab_group(function(){
+			render_tab_group(cur_group_idx);
+		})
+	})
+}
+
+render_popup();
