92118b50ccb49f8f3f48bd503decc024099d31ce
wisewolf <holo@wisewolf.org>
2016-07-09 23:28:04 +0900

    fixed bug

--- background.js
+++ background.js
@@ -6,24 +6,25 @@ function refresh_cur_tab_group(){
 				cur_group_idx= 0;
 			}
 			chrome.storage.local.get("tab_group", function(result){
-				var tab_group= result["tab_group"];
+				var temp_tab_group= result["tab_group"];
 				var new_tab_list=[];
 				var old_tab_list=[];
 				var tab_list=[];
 				tabs.forEach(function(tab){
 					new_tab_list.push(tab.url);
 				});
-				tab_group[cur_group_idx]["tab_list"].forEach(function(tab){
+				temp_tab_group[cur_group_idx]["tab_list"].forEach(function(tab){
 					old_tab_list.push(tab.url);
 				})
 				if(JSON.stringify(new_tab_list)=== JSON.stringify(old_tab_list)){
 					return;
 				}else{
 					tabs.forEach(function(tab){
-						tab_list.push(new Tab(tab.url));
+						tab_list.push(new Tab(tab.favIconUrl ,tab.url));
 					})
-					tab_group[cur_group_idx]= {"group_name":tab_group[cur_group_idx]["group_name"], "tab_list":tab_list};
-					chrome.storage.local.set({"tab_group": tab_group, "cur_group_idx": cur_group_idx});
+					temp_tab_group[cur_group_idx]= {"group_name":temp_tab_group[cur_group_idx]["group_name"], "tab_list":tab_list};
+					chrome.storage.local.set({"tab_group": temp_tab_group, "cur_group_idx": cur_group_idx});
+					tab_group= temp_tab_group;
 				}
 			});
 		});
